1a4f66d3e0982f85050a8065bca35f6f9405116e
usman0852 <dmthammad69@gmail.com>
2025-12-26 08:59:17 +0500

    v1.0

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "courier_management_system";
+
+// Create connection
+$conn = mysqli_connect($host, $user, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
